86d1f13b7d8379c7b1072383f6310a86cc9a5cb8
Lorraine <Lorraine@Isaacs-MacBook-Pro.local>
2020-08-24 23:49:44 -0500

    Deploy

--- src/actions/GiphyActions.js
+++ src/actions/GiphyActions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const apiKey = "z7EE8CMj9RXF0UzvoF7SDWMlCPOV6C8c";
+const apiKey = process.env.REACT_APP_GIPHY_KEY;
+const giphyUrl = "https://api.giphy.com/v1/gifs";
 
 export const SEARCH_GIFS = "SEARCH_GIFS";
 export const TRENDING_GIFS = "SEARCH_GIFS";
@@ -10,7 +11,7 @@ export const GIFS_ERROR = "GIFS_ERROR";
 export const searchGifs = (search = null) => async (dispatch) => {
   try {
     const res = await axios.get(
-      `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${search.replace(
+      `${giphyUrl}/search?api_key=${apiKey}&q=${search.replace(
         /\s/g,
         "+"
       )}&limit=50&offset=0&rating=g&lang=en`
@@ -31,7 +32,7 @@ export const searchGifs = (search = null) => async (dispatch) => {
 export const trendingGifs = () => async (dispatch) => {
   try {
     const res = await axios.get(
-      `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=50`
+      `${giphyUrl}/trending?api_key=${apiKey}&limit=50`
     );
 
     dispatch({
@@ -47,9 +48,7 @@ export const trendingGifs = () => async (dispatch) => {
 
 export const requestGifItem = (id = null) => async (dispatch) => {
   try {
-    const res = await axios.get(
-      `https://api.giphy.com/v1/gifs/${id}?api_key=${apiKey}`
-    );
+    const res = await axios.get(`${giphyUrl}/${id}?api_key=${apiKey}`);
 
     dispatch({
       type: "REQUEST_GIFITEM",
